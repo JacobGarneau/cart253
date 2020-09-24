e6d9d2760c8010040926b7607aa538063257fcce
Jacob Garneau <jacobg17@hotmail.fr>
2020-09-24 13:41:27 -0400

    Started exercise 1

---
+++ exercises/exercise1/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 1 - I like to move it move it
+Jacob Garneau
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(127, 255, 200);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
