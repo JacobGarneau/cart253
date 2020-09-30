b9fc4c6b28cb4e3664d02dbdcfcfb739df940c87
Jacob Garneau <jacobg17@hotmail.fr>
2020-09-30 13:46:00 -0400

    Completed video 4.4

--- Week 4/conditionals-experiments/js/script.js
+++ Week 4/conditionals-experiments/js/script.js
@@ -5,7 +5,11 @@ Jacob Garneau
 Experiments with the conditionals.
 **************************************************/
 
-let displayCircle = false;
+let caterpillar = {
+  x: 100,
+  y: 250,
+  segmentSize: 50
+}
 
 // setup()
 //
@@ -19,12 +23,27 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
+  noStroke();
+  fill(100,200,100);
 
-  if (mouseIsPressed) {
-    displayCircle = true;
-  }
+  //  WHILE METHOD
 
-  if (displayCircle) {
-    ellipse (250,250,100,100);
+  // let x = caterpillar.x;
+  // let numSegments = 10;
+  // let segmentsDrawn = 0;
+  //
+  // while(segmentsDrawn < numSegments) {
+  //   ellipse(x,caterpillar.y,caterpillar.segmentSize);
+  //   x += 40;
+  //   segmentsDrawn++;
+
+  //  FOR METHOD
+
+  let x = caterpillar.x;
+  let numSegments = 10;
+
+  for (let i = 0; i < numSegments; i++) {
+    ellipse(x,caterpillar.y,caterpillar.segmentSize);
+    x += 40;
   }
 }
