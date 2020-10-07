7bbb8aa58160b8b2d9c54a6b597a6a6f59a90367
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-07 14:13:39 -0400

    Completed video 5.1

--- Week 5/functions-experiments/js/script.js
+++ Week 5/functions-experiments/js/script.js
@@ -1,20 +1,59 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Functions experiments
+Jacob Garneau
 
-Here is a description of this template p5 project.
+Experiments with functions
 **************************************************/
+
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 1,
+  vy: 0
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  background(127, 255, 200);
+  createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  move();
+  wrap();
+  display();
+}
+
+function mousePressed() {
+  returnCircle();
+}
+
+function display() {
+  fill(255,0,0);
+  ellipse(circle.x,circle.y,circle.size);
+}
+
+function wrap() {
+  if (circle.x > width) {
+    returnCircle();
+  }
+}
+
+function move() {
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+}
+
+function returnCircle() {
+  circle.x = 0;
+  circle.vx += 2;
+  circle.vy -= 0.25;
+  circle.size += 5;
 }
