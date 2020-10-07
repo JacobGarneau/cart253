797045a862da0e969c11d427308cc2f11307e57e
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-07 14:23:27 -0400

    Completed video 5.2

--- Week 5/functions-experiments/js/script.js
+++ Week 5/functions-experiments/js/script.js
@@ -5,14 +5,6 @@ Jacob Garneau
 Experiments with functions
 **************************************************/
 
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy: 0
-}
-
 // setup()
 //
 // Description of setup() goes here.
@@ -26,34 +18,18 @@ function setup() {
 function draw() {
   background(0);
 
-  move();
-  wrap();
-  display();
+  parallels(100,100,5,1,100,1);
+  parallels(50,50,10,2,20,10);
+  parallels(200,200,15,7,3,20);
+  parallels(312,257,20,0.5,300,2);
 }
 
-function mousePressed() {
-  returnCircle();
-}
-
-function display() {
-  fill(255,0,0);
-  ellipse(circle.x,circle.y,circle.size);
-}
-
-function wrap() {
-  if (circle.x > width) {
-    returnCircle();
+function parallels(x,y,numLines,lineWidth,lineHeight,lineSpacing) {
+  for (let i = 0; i < numLines; i++) {
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    rect(x,y,lineWidth,lineHeight);
+    x += lineSpacing;
   }
-}
-
-function move() {
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-}
-
-function returnCircle() {
-  circle.x = 0;
-  circle.vx += 2;
-  circle.vy -= 0.25;
-  circle.size += 5;
 }
