79106796bb73fe580df2c02f24657d92bb38c9ba
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-07 15:01:52 -0400

    Completed video 5.6

--- Week 5/functions-experiments/js/script.js
+++ Week 5/functions-experiments/js/script.js
@@ -5,68 +5,23 @@ Jacob Garneau
 Experiments with functions
 **************************************************/
 
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  speed: 2
-};
-
-let state = `title`; // Possible states are title, animation and ending
+let bg = 0;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(500,500);
-  circle.vx = circle.speed;
-  textSize(32);
-  textAlign(CENTER,CENTER);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(0);
+  background(bg);
 
-  if (state === `title`) {
-    title();
-  } else if (state === `animation`) {
-    animation();
-  } else if (`ending`) {
-    ending();
+  if (keyIsDown(65)) {
+    rectMode(CENTER);
+    rect(250,250,100,100);
   }
-}
-
-function keyPressed() {
-  if (state === `title`) {
-    state = `animation`;
-  }
-}
-
-function title() {
-  //  Title
-  fill(255);
-  text(`Life`,width/2,height/2);
-}
-
-function animation() {
-  //  Animation
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-
-  if (circle.x > width) {
-    state = `ending`;
-  }
-
-  ellipse(circle.x,circle.y,circle.size);
-}
-
-function ending() {
-  //  Ending
-  fill(127);
-  text(`It's all over`,width/2,height/2);
 }
