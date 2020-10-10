42d333260b8efc60f3cda5a806f1a96a886fd4c9
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-10 13:55:21 -0400

    P1: Split right and wrong notes
    Split notes between right (blue) and wrong (red)
    Notes now fall from the top of the screen

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -7,7 +7,7 @@ Piano Simulator
 
 let note = {
   x: 0,
-  y: 500,
+  y: 0,
   size: 0,
   fill: {
     r: 0,
@@ -22,7 +22,9 @@ let note = {
 let numWhiteKeys = 21;
 let keyboardHeight = 300;
 
-let notePosition = [];
+let rightNotes = [];
+let wrongNotes = [];
+let rightPercent = 90;
 
 let scaleNotes = {
   major: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
@@ -39,8 +41,8 @@ function setup() {
 
   createScale(scaleNotes.pentatonic);
 
-  note.size = width / numWhiteKeys;
-  note.x = random(notePosition) * width / numWhiteKeys + note.size / 2;
+  placeNote();
+
   note.vy = note.speed;
 }
 
@@ -50,17 +52,21 @@ function setup() {
 function draw() {
   background(127, 255, 200);
 
-  keyboard();
-
   note.y += note.vy;
 
   push();
-  fill(0,127,255);
+  fill(note.fill.r,note.fill.g,note.fill.b);
   noStroke();
   ellipse(note.x,note.y,note.size);
   pop();
+
+  keyboard();
+
+  detectNoteHeight();
 }
 
+//  keyboard()
+//  Draws the black and white keys of the keyboard
 function keyboard() {
   //  Draw the white keys
   fill(255);
@@ -72,8 +78,9 @@ function keyboard() {
   fill(0);
   for (let i = 0; i < numWhiteKeys; i++) {
     if (i === 2 || i === 6 || i === 9 || i === 13 || i === 16 || i === 20) {
-
+      //  These spaces do not have black keys
     } else {
+      //  These spaces have black keys
       rect(0 + i * width / numWhiteKeys + width / numWhiteKeys / 1.5,
           height - keyboardHeight,
           width / numWhiteKeys - width / numWhiteKeys / 3,
@@ -82,45 +89,80 @@ function keyboard() {
   }
 }
 
+//  createScale()
+//  Splits note positions between right and wrong notes depending on the musical scale
 function createScale(scale) {
   for (let i = 0; i < numWhiteKeys + numWhiteKeys / 7 * 5; i++) {
 
     if (scale.includes(i)) {
-      //  If the note is not part of the scale, make it blue
-      note.fill.r = 255;
-      note.fill.g = 127;
-      note.fill.b = 0;
-
-      createNotePosition(i);
-
+      //  If the note is part of the scale, add it to the right notes array
+      adjustNotePosition(i,rightNotes);
     } else {
-      //  If the note is not in the scale, make it red
-      note.fill.r = 255;
-      note.fill.g = 127;
-      note.fill.b = 0;
-
+      //  If the note is not part of the scale, add it to the wrong notes array
+      adjustNotePosition(i,wrongNotes);
     }
   }
 }
 
-function createNotePosition(position) {
+//  adjustNotePosition()
+//  Adjust the note position so that they are always aligned with a key (especially the black ones, which are not regular)
+function adjustNotePosition(position,array) {
   if (position <= 20) {
-
-    notePosition.push(position);
+    //  If the note position is on a white key, leave it as is
+    array.push(position);
   } else {
-    
+    //  If the note position is on a blak key, adjust it so it is properly aligned
     if (position < 23) {
-      notePosition.push(position - 21 + 0.5);
+      array.push(position - 21 + 0.5);
     } else if (position > 22 && position < 26) {
-      notePosition.push(position - 21 + 1.5);
+      array.push(position - 21 + 1.5);
     } else if (position > 25 && position < 28) {
-      notePosition.push(position - 21 + 2.5);
+      array.push(position - 21 + 2.5);
     } else if (position > 27 && position < 31) {
-      notePosition.push(position - 21 + 3.5);
+      array.push(position - 21 + 3.5);
     } else if (position > 30 && position < 33) {
-      notePosition.push(position - 21 + 4.5);
+      array.push(position - 21 + 4.5);
     } else if (position > 32) {
-      notePosition.push(position - 21 + 5.5);
+      array.push(position - 21 + 5.5);
     }
+  }
+}
+
+//  placeNote()
+//  Places notes on the screen and gives them the appropriate color
+function placeNote() {
+  note.y = -note.size;
+
+  //  Change the note color to blue or red depending on if it is right or wrong
+  if (random(1,100) <= rightPercent) {
+    note.fill.r = 0;
+    note.fill.g = 127;
+    note.fill.b = 255;
+    note.x = random(rightNotes) * width / numWhiteKeys;
+  } else {
+    note.fill.r = 255;
+    note.fill.g = 20;
+    note.fill.b = 0;
+    note.x = random(wrongNotes) * width / numWhiteKeys;
+  }
+
+  //  Make notes on black keys smaller than those on white keys
+  // if (note.x > 20 / width * numWhiteKeys) {
+  //   note.size = width / numWhiteKeys - width / numWhiteKeys / 3;
+  // } else {
+  //   note.size = width / numWhiteKeys;
+  // }
+
+  note.size = width / numWhiteKeys;
+
+  //  Center the note in relation to the key
+  note.x += note.size / 2;
+}
+
+//  detectNoteHeight()
+//  Detects if notes have reached the keyboard
+function detectNoteHeight() {
+  if (note.y >= height - keyboardHeight + note.size / 2) {
+    placeNote();
   }
 }
