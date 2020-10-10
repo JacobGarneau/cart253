144b3063a58814e65e67c955260e45896c961f19
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-10 10:46:17 -0400

    P1: Added keyboard
    Added the black and white keys of the keyboard
    The width of the keys is relative to the width of the screen

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -5,6 +5,9 @@ Jacob Garneau
 Piano Simulator
 **************************************************/
 
+let numWhiteKeys = 24;
+let keyboardHeight = 300;
+
 // setup()
 //
 // Description of setup() goes here.
@@ -19,8 +22,26 @@ function setup() {
 function draw() {
   background(127, 255, 200);
 
+  //  Draw the white keys
   fill(255);
-  for (let i = 0; i < 20; i++) {
-    rect(0 + i * width / 20,height - 300,width / 20,300);
+  for (let i = 0; i < numWhiteKeys; i++) {
+    rect(0 + i * width / numWhiteKeys,height - keyboardHeight,width / numWhiteKeys,keyboardHeight);
   }
+
+  //  Draw the black keys
+  fill(0);
+  for (let i = 0; i < numWhiteKeys; i++) {
+    if (i === 2 || i === 6 || i === 9 || i === 13 || i === 16 || i === 20 || i === 23) {
+
+    } else {
+      rect(0 + i * width / numWhiteKeys + width / numWhiteKeys / 1.5,
+          height - keyboardHeight,
+          width / numWhiteKeys - width / numWhiteKeys / 3,
+          keyboardHeight / 2);
+    }
+  }
+
+
+
+
 }
