3be3ab03a9981154257da3c118a8940556c52a2e
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-21 15:08:33 -0400

    Completed video 7.2

--- Week 6/week6-experiments/js/script.js
+++ Week 6/week6-experiments/js/script.js
@@ -7,19 +7,8 @@ Here is a description of this template p5 project.
 
 "use strict";
 
-let user = {
-  x: 0,
-  y: 0,
-  size: 100
-};
-
-
-let food1;
-let food2;
-let food3;
-let food4;
-let food5;
-let food6;
+let school = [];
+let schoolSize = 10;
 
 // setup()
 //
@@ -27,22 +16,22 @@ let food6;
 function setup() {
   createCanvas(windowWidth,windowHeight);
 
-  food1 = createFood(250,windowHeight / 2);
-  food2 = createFood(350,windowHeight / 2);
-  food3 = createFood(450,windowHeight / 2);
-  food4 = createFood(550,windowHeight / 2);
-  food5 = createFood(650,windowHeight / 2);
-  food6 = createFood(750,windowHeight / 2);
+  for (let i = 0; i < schoolSize; i++) {
+    let fish = createFish(random(0,width / 2),random(0,height));
+    school.push(fish);
+  }
 }
 
-function createFood(x,y) {
-  let food = {
+function createFish(x,y) {
+  let fish = {
     x: x,
     y: y,
     size: 50,
-    eaten: false
-  };
-  return food;
+    vx: 0,
+    vy: 0,
+    speed: 2
+  }
+  return fish;
 }
 
 // draw()
@@ -51,51 +40,37 @@ function createFood(x,y) {
 function draw() {
   background(0);
 
-  moveUser();
-
-  checkFood(food1);
-  checkFood(food2);
-  checkFood(food3);
-  checkFood(food4);
-  checkFood(food5);
-  checkFood(food6);
-
-  displayUser();
-
-  displayFood(food1);
-  displayFood(food2);
-  displayFood(food3);
-  displayFood(food4);
-  displayFood(food5);
-  displayFood(food6);
-}
-
-function moveUser() {
-  user.x = mouseX;
-  user.y = mouseY;
-}
-
-function checkFood(food) {
-  if(!food.eaten) {
-    let d = dist(user.x,user.y,food.x,food.y);
-    if (d < user.size / 2 + food.size / 2) {
-      food.eaten = true;
-    }
+  for (let i = 0; i < school.length; i++) {
+    moveFish(school[i]);
+    displayFish(school[i]);
   }
 }
 
-function displayUser() {
-  push();
-  fill(255);
-  ellipse(user.x,user.y,user.size);
-  pop();
+function moveFish(fish) {
+  let change = random(0,1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed,fish.speed);
+    fish.vy = random(-fish.speed,fish.speed);
+  }
+
+  fish.x += fish.vx;
+  fish.y += fish.vy;
+
+  fish.x = constrain(fish.x,0,width);
+  fish.y = constrain(fish.y,0,height);
 }
 
-function displayFood(food) {
-  if (!food.eaten) {
+function displayFish(fish) {
+  if (!fish.eaten) {
     push();
-    fill(255,100,100);
-    ellipse(food.x,food.y,food.size);
+    fill(200,100,100);
+    noStroke();
+    ellipse(fish.x,fish.y,fish.size);
     pop();
   }
+}
+
+function mousePressed() {
+  let fish = createFish(mouseX,mouseY);
+  school.push(fish);
 }
