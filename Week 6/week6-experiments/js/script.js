2392eab32797d53db3f70018ec1c503b76f11fc4
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-21 15:28:19 -0400

    Completed video 7.3

--- Week 6/week6-experiments/js/script.js
+++ Week 6/week6-experiments/js/script.js
@@ -7,70 +7,24 @@ Here is a description of this template p5 project.
 
 "use strict";
 
-let school = [];
-let schoolSize = 10;
+let barkSFX;
 
-// setup()
-//
-// Description of setup() goes here.
+let rates = [1.5,1.75,2.25,2.5,2.75,3];
+
+function preload() {
+  barkSFX = loadSound(`assets/sounds/bark.wav`);
+}
+
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-
-  for (let i = 0; i < schoolSize; i++) {
-    let fish = createFish(random(0,width / 2),random(0,height));
-    school.push(fish);
-  }
+  createCanvas(600,600);
 }
 
-function createFish(x,y) {
-  let fish = {
-    x: x,
-    y: y,
-    size: 50,
-    vx: 0,
-    vy: 0,
-    speed: 2
-  }
-  return fish;
-}
-
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
   background(0);
-
-  for (let i = 0; i < school.length; i++) {
-    moveFish(school[i]);
-    displayFish(school[i]);
-  }
-}
-
-function moveFish(fish) {
-  let change = random(0,1);
-  if (change < 0.05) {
-    fish.vx = random(-fish.speed,fish.speed);
-    fish.vy = random(-fish.speed,fish.speed);
-  }
-
-  fish.x += fish.vx;
-  fish.y += fish.vy;
-
-  fish.x = constrain(fish.x,0,width);
-  fish.y = constrain(fish.y,0,height);
-}
-
-function displayFish(fish) {
-  if (!fish.eaten) {
-    push();
-    fill(200,100,100);
-    noStroke();
-    ellipse(fish.x,fish.y,fish.size);
-    pop();
-  }
 }
 
 function mousePressed() {
-  let fish = createFish(mouseX,mouseY);
-  school.push(fish);
+  let randomRate = random(rates);
+  barkSFX.rate(randomRate);
+  barkSFX.play();
 }
