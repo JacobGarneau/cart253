c75bc81911ad1fc3ba5ce2cfb0a53b3c13f53e8c
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-21 14:52:16 -0400

    Completed video 7.1

---
+++ Week 6/week6-experiments/js/script.js
@@ -0,0 +1,101 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+"use strict";
+
+let user = {
+  x: 0,
+  y: 0,
+  size: 100
+};
+
+
+let food1;
+let food2;
+let food3;
+let food4;
+let food5;
+let food6;
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+  food1 = createFood(250,windowHeight / 2);
+  food2 = createFood(350,windowHeight / 2);
+  food3 = createFood(450,windowHeight / 2);
+  food4 = createFood(550,windowHeight / 2);
+  food5 = createFood(650,windowHeight / 2);
+  food6 = createFood(750,windowHeight / 2);
+}
+
+function createFood(x,y) {
+  let food = {
+    x: x,
+    y: y,
+    size: 50,
+    eaten: false
+  };
+  return food;
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+  background(0);
+
+  moveUser();
+
+  checkFood(food1);
+  checkFood(food2);
+  checkFood(food3);
+  checkFood(food4);
+  checkFood(food5);
+  checkFood(food6);
+
+  displayUser();
+
+  displayFood(food1);
+  displayFood(food2);
+  displayFood(food3);
+  displayFood(food4);
+  displayFood(food5);
+  displayFood(food6);
+}
+
+function moveUser() {
+  user.x = mouseX;
+  user.y = mouseY;
+}
+
+function checkFood(food) {
+  if(!food.eaten) {
+    let d = dist(user.x,user.y,food.x,food.y);
+    if (d < user.size / 2 + food.size / 2) {
+      food.eaten = true;
+    }
+  }
+}
+
+function displayUser() {
+  push();
+  fill(255);
+  ellipse(user.x,user.y,user.size);
+  pop();
+}
+
+function displayFood(food) {
+  if (!food.eaten) {
+    push();
+    fill(255,100,100);
+    ellipse(food.x,food.y,food.size);
+    pop();
+  }
+}
