a288a3b2435e39c18cd699a62b91de7773209a4d
Jacob Garneau <jacobg17@hotmail.fr>
2020-10-28 16:26:49 -0400

    E5: Started exercise 5
    Started exercise 5: Juggle Garden

---
+++ exercises/exercise5/js/script.js
@@ -0,0 +1,18 @@
+/**************************************************
+Exercise 5 - Juggle Garden
+Jacob Garneau
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(127, 255, 200);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
