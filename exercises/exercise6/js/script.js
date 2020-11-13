7c6bc277bc04381371e95fd78169484b2e2f3551
Jacob Garneau <jacobg17@hotmail.fr>
2020-11-12 20:37:29 -0500

    E6: Added dynamic music
    The music will switch seemlessly between three tracks depending on where the user's mouse is

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -2,6 +2,7 @@
 Exercise 6 - Make Some Noise
 Jacob Garneau
 
+Since the songs are not of the same length, there will be a moment of silence in the "forest" song until the others end.
 All music was made by me.
 **************************************************/
 
@@ -16,8 +17,8 @@ let music = {
 
 function preload() {
   music.desert = loadSound(`assets/sounds/desert_music.mp3`);
-  music.cold = loadSound(`assets/sounds/desert_music.mp3`);
-  music.forest = loadSound(`assets/sounds/desert_music.mp3`);
+  music.cold = loadSound(`assets/sounds/cold_music.mp3`);
+  music.forest = loadSound(`assets/sounds/forest_music.mp3`);
 }
 
 // setup()
@@ -26,6 +27,16 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   user = new User();
+
+  music.desert.setVolume(0, 0);
+  music.cold.setVolume(0, 0);
+  music.forest.setVolume(0, 0);
+
+  playMusic();
+  setInterval(playMusic(), 98000);
+}
+
+function playMusic() {
   music.desert.play();
   music.cold.play();
   music.forest.play();
@@ -36,26 +47,59 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
+  noStroke();
 
+  fill(255, 200, 0);
   rect(0, 0, width / 2, height / 2);
+  fill(0, 200, 255);
+  rect(0, height / 2, width / 2, height / 2);
+  fill(100, 200, 100);
   rect(width / 2, height / 2, width / 2, height / 2);
 
-  for (let i = 0; i < balls.length; i++) {
-    balls[i].move();
-    balls[i].bounce();
-    balls[i].display();
+  fill(255);
+  ellipse(mouseX, mouseY, user.size);
+
+  checkEnvironment();
+}
+
+function checkEnvironment() {
+  let dX = dist(mouseX, 0, width / 2, 0);
+  let dY = dist(0, mouseY, 0, height / 2);
+
+  //  Trigger desert music
+  if (
+    (dX < user.size && mouseY < height / 2) ||
+    (dY < user.size && mouseX < width / 2)
+  ) {
+    music.desert.setVolume(1, 1);
+  } else if (mouseX < width / 2 && mouseY < height / 2) {
+  } else {
+    music.desert.setVolume(0, 0.5);
+  }
+
+  //  Trigger cold music
+  if (
+    (dX < user.size && mouseY > height / 2) ||
+    (dY < user.size && mouseX < width / 2)
+  ) {
+    music.cold.setVolume(1, 1);
+  } else if (mouseX < width / 2 && mouseY > height / 2) {
+  } else {
+    music.cold.setVolume(0, 0.5);
+  }
+
+  //  Trigger forest music
+  if (
+    (dX < user.size && mouseY > height / 2) ||
+    (dY < user.size && mouseX > width / 2)
+  ) {
+    music.forest.setVolume(1, 1);
+  } else if (mouseX > width / 2 && mouseY > height / 2) {
+  } else {
+    music.forest.setVolume(0, 0.5);
   }
 }
 
 function mousePressed() {
-  if (randomNumber < 33) {
-    let scale = notes.major;
-  } else if ((randomNumber >= 33) & (randomNumber < 66)) {
-    let scale = notes.minor;
-  } else if (randomNumber >= 66) {
-    let scale = notes.pentatonic;
-  }
-
-  createBall(mouseX, mouseY, random(scale));
   userStartAudio();
 }
